ec0acf6c60b2f84eede11778ab0396283ecc9e44
JCKalt <ckaltwasser@reminingcorp.com>
2023-03-15 10:54:10 -0400

    added verification

--- put-item.sh
+++ put-item.sh
@@ -1,1 +1,3 @@
-curl -X "PUT" -H "Content-Type: application/json" -d "{\"id\": \"123\", \"price\": 12345, \"name\": \"myitem\"}" https://7jdqys3801.execute-api.us-east-2.amazonaws.com/items
+curl -X "PUT" -H "Content-Type: application/json" \
+   -H "Authorization: Bearer a8cbdc83bad52ed9b2f1a59fc5833b19" \
+   -d "{\"id\": \"123\", \"price\": 12345, \"name\": \"myitem\"}" https://7jdqys3801.execute-api.us-east-2.amazonaws.com/items
