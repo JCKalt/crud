483aaa50307f1c46b505e052ddf6413b03a022dd
JCKalt <ckaltwasser@reminingcorp.com>
2023-03-15 10:29:18 -0400

    added secret check and log

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -13,6 +13,7 @@ const client = new DynamoDBClient({});
 const dynamo = DynamoDBDocumentClient.from(client);
 
 const tableName = "http-crud-tutorial-items";
+const secretKey = "a8cbdc83bad52ed9b2f1a59fc5833b19";
 
 export const handler = async (event, context) => {
   let body;
@@ -20,8 +21,40 @@ export const handler = async (event, context) => {
   const headers = {
     "Content-Type": "application/json",
   };
+  let secretStatus = "Secret key not provided or invalid.";
+  console.log("Headers:", event.headers);
+
+  const authHeader = event.headers.Authorization || event.headers.authorization;
+  if (!authHeader) {
+    console.log("Check1: Authorization header is missing");
+  } else {
+    console.log("Check2: Authorization header is present");
+  }
 
   try {
+    if (
+      authHeader
+      ) {
+      console.log("Authorization.header -->", authHeader);
+      const apart1 = authHeader.split(" ")[0].toLowerCase();
+      console.log("apart1 -->", apart1);
+      if (apart1 === "bearer") {
+        const apart2 = authHeader.split(" ")[1];
+        console.log("apart2 -->", apart2);
+        if (apart2 === secretKey.toLowerCase()) {
+            secretStatus = "Secret key validated successfully.";
+            console.log("key good");
+        } else {
+            secretStatus = "Invalid secret key provided.";
+            console.log("key bad");
+        }
+      }
+    }
+    else {
+      console.log("Check 2: Authorization header is missing");
+    }
+    console.log("SecretStatus:", secretStatus);
+
     switch (event.routeKey) {
       case "DELETE /items/{id}":
         await dynamo.send(
