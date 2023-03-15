df74d39ddc226eb4a73641e69ecd63e02ee67841
JCKalt <ckaltwasser@reminingcorp.com>
2023-03-15 10:20:14 -0400

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # crud
+Work for AWS API crud tutorial found here: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-dynamo-db.html#http-api-dynamo-db-create-table
---
+++ put-item.sh
@@ -0,0 +1,1 @@
+curl -X "PUT" -H "Content-Type: application/json" -d "{\"id\": \"123\", \"price\": 12345, \"name\": \"myitem\"}" https://7jdqys3801.execute-api.us-east-2.amazonaws.com/items
